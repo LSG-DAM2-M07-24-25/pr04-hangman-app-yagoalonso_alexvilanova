380f2de34c3f5e161dadde6dd57e32ef1acb1247
yagoalonso1 <yagoalonso3105@gmail.com>
2024-12-09 15:57:08 +0100

    add Difficulty and Game Model

---
+++ app/src/main/java/com/example/pr4/model/Difficulty.kt
@@ -0,0 +1,5 @@
+package com.example.pr4.models
+
+enum class Difficulty {
+    EASY, NORMAL, HARD
+}
---
+++ app/src/main/java/com/example/pr4/model/GameModel.kt
@@ -0,0 +1,12 @@
+package com.example.pr4.models
+
+data class GameModel(
+    val secretWord: String = "",
+    val revealedWord: String = "",
+    val attempts: Int = 0,
+    val maxAttempts: Int = 7,
+    val usedLetters: Set<Char> = emptySet(),
+    val isGameWon: Boolean = false,
+    val isGameLost: Boolean = false,
+    val difficulty: Difficulty = Difficulty.NORMAL
+)
