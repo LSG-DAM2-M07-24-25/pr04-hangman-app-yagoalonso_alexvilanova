e3beef1d151e4d6dccb14defd23faaa23a35bf84
yagoalonso1 <yagoalonso3105@gmail.com>
2024-12-09 16:19:23 +0100

    Add: MenuViewModel.kt

---
+++ app/src/main/java/com/example/pr4/modelview/MenuViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.pr4.viewmodels
+
+import androidx.lifecycle.ViewModel
+import com.example.pr4.models.Difficulty
+import com.example.pr4.models.MenuModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class MenuViewModel : ViewModel() {
+    private val _menuState = MutableStateFlow(MenuModel())
+    val menuState = _menuState.asStateFlow()
+
+    fun selectDifficulty(difficulty: Difficulty) {
+        _menuState.update { it.copy(selectedDifficulty = difficulty) }
+    }
+}
